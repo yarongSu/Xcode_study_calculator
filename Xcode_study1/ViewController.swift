2d387a0ece4fd0c03372a85e28e711aecda2e899
yarongSu <huasuyarong@163.com>
2018-08-05 23:41:59 +0800

    in coding

--- Xcode_study1/ViewController.swift
+++ Xcode_study1/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Result_lable: UILabel!
+    var userinput:Bool=false
+    
+    @IBAction func adddigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if (userinput){
+            Result_lable.text = Result_lable.text! + digit
+        }else{
+            Result_lable.text=digit
+            userinput=true
+                
+        }
+    }
+  
+ 
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userinput{
+            enter()
+        }
+        switch operation{
+        case "*":performace
+        }
+    }
+    
+    func performanceOperation(operation:(Double,Double)->Double){
+        if 
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
